cee1cdbecc1866a16f10d290f10920c6839e61b8
Esquivel Morales Mario Alberto <91237702+riotoveles@users.noreply.github.com>
2021-11-03 13:00:23 -0600

    Add files via upload

---
+++ Rentas.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>11 Esquivel Mario</title>
+    
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/logo4.png" alt="FalconMasters"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="empleado.html">Empleado</a></li>
+					<li><a href="Computadora.php">Computadora</a></li>
+					<li><a href="Rentas.php">Rentas</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Bienvenidos! al Cibercafe Esquivel</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<p>En Cibercafe Esquivel nos preocupamos por darte un buen servicio.</p>
+					<br/>
+					<p> Las computadoras son tu mundo y tambien es el nuestro! </p>
+					<br/>
+					<p>Acompañanos en nuestros diferentes locales, tenemos computadoras para cualquier tipo de tarea que necesites</p>
+					<br/>
+					<p>Ven y descubre todo lo que tenemos en Cibercafe Esquivel.
+                    Un cibercafé, ciber café, café internet o solo ciber, es un local público donde se ofrece a los clientes acceso a internet y, aunque no en todos, también servicios de cafetería, bar o restaurante.
+                    </p>
+				</article>
+			</div>
+
+			<aside>
+				<div class="widget">
+					<h3>Listado Tablas en Cibercafe Esquivel</h3>
+					<ul>
+						<li><a href="empleado.html">Tabla 1 Empleado</a></li>
+						<li><a href="Computadora.php">Tabla 2 Computadora</a></li>
+						<li><a href="Rentas.php">Tabla 3 Renta</a></li>
+						<li><a href="mapa.html">Tabla 4 Ubicacion</a></li>
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	<footer>
+		<div class="wrapp">
+            <p> No. lista 11 </p>
+            <p> SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/riotoveles/proyectofinal_daweb</p>
+		</div>
+	</footer>
+</body>
+</html>
